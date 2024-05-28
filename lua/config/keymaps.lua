2cf8303ea624a86b3ad47a04bb3e1c9e63504012
Daniel Knight <danny.kulangiev@gmail.com>
2024-05-28 14:13:47 -0400

    update config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,17 +4,21 @@
 
 local map = vim.keymap.set
 
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
+
 map("x", "p", '"_dP')
 
 map("i", "jk", "<Esc>", { noremap = true, silent = true })
+
+-- shift h/l to move to beginning/end of line
+map({ "n", "v" }, "H", "^")
+map({ "n", "v" }, "L", "$")
 
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
 map("n", "n", "nzzzv")
 map("n", "N", "Nzzzv")
-
-map({ "n", "v" }, "<S-h>", "^", { desc = "Start of line" })
-map({ "n", "v" }, "<S-l>", "$", { desc = "End of line" })
 
 map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { silent = true })
 map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { silent = true })
@@ -22,3 +26,14 @@ map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { silent = true })
 map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { silent = true })
 
 map("n", "<leader>\\", "<C-W>v", { desc = "Split Window Right", remap = true })
+
+map("n", "<leader>bd", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
+map("n", "<leader>bD", "<cmd> bufdo bd<CR>", { desc = "Delete all buffers", remap = true })
+
+-- Move line up/down in visual mode
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Create new empty line above/below
+map("n", "<Enter>", "o<ESC>")
+map("n", "<S-Enter>", "O<ESC>")
