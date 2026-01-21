be2ef50fcb388be17cd163c603910b83b574d3aa
Your Name <danny.kulangiev@gmail.com>
2026-01-21 11:17:43 -0500

    fix: re-enable smear-cursor (not causing crashes)

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,7 +2,6 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "lewis6991/gitsigns.nvim", enabled = false },
-  { "sphamba/smear-cursor.nvim", enabled = false },
   {
     "folke/snacks.nvim",
     opts = {
