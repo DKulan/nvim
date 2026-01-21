0235847b141eaa01a009e21b570c9b16dee809aa
Your Name <danny.kulangiev@gmail.com>
2026-01-21 11:15:25 -0500

    fix: disable snacks async features causing TUI crashes in tmux
    - Disable snacks animate/scroll/notifier/picker (nvim 0.11 + tmux issue)
    - Switch from snacks_picker to telescope
    - Remove claudecode and smear-cursor extras
    - Enable yazi for directories

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,14 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "lewis6991/gitsigns.nvim", enabled = false },
+  { "sphamba/smear-cursor.nvim", enabled = false },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      animate = { enabled = false },
+      scroll = { enabled = false },
+      notifier = { enabled = false },
+      picker = { enabled = false },
+    },
+  },
 }
